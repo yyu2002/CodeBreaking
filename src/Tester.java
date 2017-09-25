40d30898da90ac1464d836ae25467b8819d52956
David Dobervich <david.dobervich@gmail.com>
2017-09-25 16:06:31 -0700

    setup tester

--- src/Tester.java
+++ src/Tester.java
@@ -2,11 +2,11 @@
 public class Tester {
 
 	public static void main(String[] args) {
-		String plaintext = "the quick brown fox jumped over the lazy dogs";
-		String cipherText = Cipher.rotationCipherEncrypt(plaintext, 3, Cipher.ALPHABET);
-
-		System.out.println("Plaintext: " + plaintext);
-		System.out.println("Cipertext: " + cipherText);
+		String cipherText = Cipher.loadFileAsString("cipherText1.txt");
+		System.out.println("CIPHER TEXT IS:\n" + cipherText);
+		
+		
+		
 	}
 
 }
