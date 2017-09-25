1d48f769ae91441c31edb00a2edc1ab54cd884ea
David Dobervich <david.dobervich@gmail.com>
2017-09-25 16:49:41 -0700

    add testers

---
+++ src/DictionaryTester.java
@@ -0,0 +1,13 @@
+
+public class DictionaryTester {
+
+	public static void main(String[] args) {
+		Dictionary dict = Dictionary.buildDictionary("text/words.txt");
+		System.out.println(dict.isWord("hello")); // yes a word!
+		System.out.println(dict.isWord("Hello")); // NOTE: our dictionary file
+													// has only lower-case
+													// words!
+		System.out.println(dict.isWord("jeifv")); // this is not a word
+	}
+
+}
--- src/Tester.java
+++ src/Tester.java
@@ -2,12 +2,49 @@
 public class Tester {
 
 	public static void main(String[] args) {
-		String cipherText = Cipher.loadFileAsString("plainText2.txt");
-		System.out.println("TEXT IS:\n" + cipherText);
+		String cipherText, plainText, password;
 		
-		String text = Cipher.rotationCipherEncrypt(cipherText, 20);
+		// -========== CRACK CIPHER 1 ===========-
+		// cipherText1.txt was encoded with a rotation cipher
+		// -=====================================-
+		
+		cipherText = Cipher.loadFileAsString("cipherText1.txt");
+		System.out.println("Cipher text 1:\n" + cipherText);
 
-		String plainText = Cipher.rotationCipherDecrypt(text, 20);
-		System.out.println("PLAINTEXT IS: \n" + plainText);
+		int rotAmount = Cipher.bruteForceCrackRotation(cipherText);
+		System.out.println("CRACKED! Rotation amount is " + rotAmount);
+		
+		plainText = Cipher.rotationCipherDecrypt(cipherText, rotAmount);
+		
+		System.out.println("PLAINTEXT IS: " + plainText);		
+		
+		
+		// -========== CRACK CIPHER 2 ===========-
+		// cipherText2.txt was encoded with a 2-letter password
+		// ---------------------------------------
+		
+//		cipherText = Cipher.loadFileAsString("cipherText2.txt");
+//		System.out.println("Cipher text 2:\n" + cipherText);
+//
+//		password = Cipher.bruteForceCrackLength2Password(cipherText);
+//		System.out.println("CRACKED! Rotation amount is " + rotAmount);
+//		
+//		plainText = Cipher.rotationCipherDecrypt(cipherText, rotAmount);
+//		
+//		System.out.println("PLAINTEXT IS: " + plainText);
+		
+		// -========== CRACK CIPHER 3 ===========-
+		// cipherText2.txt was encoded with a 3-letter password
+		// ---------------------------------------
+		
+//		cipherText = Cipher.loadFileAsString("cipherText2.txt");
+//		System.out.println("Cipher text 3:\n" + cipherText);
+//
+//		password = Cipher.bruteForceCrackLength2Password(cipherText);
+//		System.out.println("CRACKED! Rotation amount is " + rotAmount);
+//		
+//		plainText = Cipher.rotationCipherDecrypt(cipherText, rotAmount);
+//		
+//		System.out.println("PLAINTEXT IS: " + plainText);
 	}
 }
