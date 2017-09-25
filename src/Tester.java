df7ab26a5dfd628549dbcfb6ee5531f4bff9fbe5
David Dobervich <david.dobervich@gmail.com>
2017-09-25 16:08:36 -0700

    finish setting up tester

--- src/Tester.java
+++ src/Tester.java
@@ -5,8 +5,8 @@ public class Tester {
 		String cipherText = Cipher.loadFileAsString("cipherText1.txt");
 		System.out.println("CIPHER TEXT IS:\n" + cipherText);
 		
-		
+		String plainText = Cipher.rotationCipherDecrypt(cipherText, -20);
+		System.out.println("PLAINTEXT IS: \n" + plainText);
 		
 	}
-
 }
